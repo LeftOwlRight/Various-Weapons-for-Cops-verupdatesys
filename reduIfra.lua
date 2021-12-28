845cd465401e2194fb26ed407d8659b2183d78a4
LeftOwlRight <78366427+LeftOwlRight@users.noreply.github.com>
2021-12-28 19:40:04 +0800

    Try to fix a PlayerDamage crash

--- reduIfra.lua
+++ reduIfra.lua
@@ -3,7 +3,6 @@ if StreamHeist then
 end
 
 function PlayerDamage:_chk_dmg_too_soon(damage, ...)
-	if VariousWeaponForCops.settings.Reduce_Iframe then
 	local next_allowed_dmg_t = type(self._next_allowed_dmg_t) == "number" and self._next_allowed_dmg_t or Application:digest_value(self._next_allowed_dmg_t, false)
 	local t = managers.player:player_timer():time()
 	if damage <= self._last_received_dmg + 0.01 and next_allowed_dmg_t > t then
@@ -15,27 +14,26 @@ function PlayerDamage:_chk_dmg_too_soon(damage, ...)
 		self._old_last_received_dmg = self._last_received_dmg
 		self._old_next_allowed_dmg_t = next_allowed_dmg_t
 	end
-    end
 end
 
 local _calc_armor_damage_original = PlayerDamage._calc_armor_damage
 function PlayerDamage:_calc_armor_damage(attack_data, ...)
-	if VariousWeaponForCops.settings.Reduce_Iframe then
+	if VariousWeaponForCops and VariousWeaponForCops.settings.Reduce_Iframe then
 	attack_data.damage = attack_data.damage - (self._old_last_received_dmg or 0)
 	self._next_allowed_dmg_t = self._old_next_allowed_dmg_t and Application:digest_value(self._old_next_allowed_dmg_t, true) or self._next_allowed_dmg_t
 	self._old_last_received_dmg = nil
 	self._old_next_allowed_dmg_t = nil
-	return _calc_armor_damage_original(self, attack_data, ...)
     end
+    return _calc_armor_damage_original(self, attack_data, ...)
 end
 
 local _calc_health_damage_original = PlayerDamage._calc_health_damage
 function PlayerDamage:_calc_health_damage(attack_data, ...)
-	if VariousWeaponForCops.settings.Reduce_Iframe then
+	if VariousWeaponForCops and VariousWeaponForCops.settings.Reduce_Iframe then
 	attack_data.damage = attack_data.damage - (self._old_last_received_dmg or 0)
 	self._next_allowed_dmg_t = self._old_next_allowed_dmg_t and Application:digest_value(self._old_next_allowed_dmg_t, true) or self._next_allowed_dmg_t
 	self._old_last_received_dmg = nil
 	self._old_next_allowed_dmg_t = nil
-	return _calc_health_damage_original(self, attack_data, ...)
     end
+    return _calc_health_damage_original(self, attack_data, ...)
 end
