8787d9def8e55b3eb2b1caadc2f7f276acd71d92
LR_Daring <78366427+LeftOwlRight@users.noreply.github.com>
2022-01-01 07:32:32 +0800

    v3.0

--- LtyR.lua
+++ LtyR.lua
@@ -3,15 +3,21 @@ dofile(ModPath .. "automenubuilder.lua")
 VariousWeaponForCops = VariousWeaponForCops or {
 	settings = {
 		Dangerous_mode = false,
+		Risk_factor = 5,
 		No_Zeal_Shotgunner = false,
 		Reduce_Iframe = false,
+		Data_differs = false,
+		Dont_Be_Annoying = true,
 		Openmod = true
-	}
+	},
+	values = {
+		    Risk_factor = { 10, 250, 1 }
+		  }
   }
 
 Hooks:Add("MenuManagerBuildCustomMenus", "MenuManagerBuildCustomMenusRandomWeapon", function(menu_manager, nodes)
 	AutoMenuBuilder:load_settings(VariousWeaponForCops.settings, "various_weapon_for_cops")
-	AutoMenuBuilder:create_menu_from_table(nodes, VariousWeaponForCops.settings, "various_weapon_for_cops", "blt_options")
+	AutoMenuBuilder:create_menu_from_table(nodes, VariousWeaponForCops.settings, "various_weapon_for_cops", "blt_options",VariousWeaponForCops.values)
 end)
 
 local mpath=ModPath
@@ -34,10 +40,12 @@ function CopBase:default_weapon_name()
 	-- if not VariousWeaponForCops.settings.Openmod then
 	-- 	managers.chat:send_message(1, peer, 'mod disabled')
 	-- end
-	tweak_data.weapon.raging_bull_npc.DAMAGE = 6
-	tweak_data.weapon.mini_npc.auto.fire_rate = 0.01
-	tweak_data.weapon.m249_npc.auto.fire_rate = 0.06
-	tweak_data.weapon.ak47_ass_npc.auto.fire_rate = 0.18
+	if VariousWeaponForCops.settings.Data_differs then
+	    tweak_data.weapon.raging_bull_npc.DAMAGE = 6
+	    --tweak_data.weapon.mini_npc.auto.fire_rate = 0.01
+	    --tweak_data.weapon.m249_npc.auto.fire_rate = 0.06
+	    tweak_data.weapon.ak47_ass_npc.auto.fire_rate = 0.18
+	end
 	--local weight = math.random(1, 1000)
 	--local a={weaponid}, b=math.random(#a)
 	--Utils:IsCurrentWeapon(weapontype)
@@ -75,8 +83,92 @@ local function random_weapon()
     end
 end
 
-if VariousWeaponForCops.settings.No_Zeal_Shotgunner then
+if VariousWeaponForCops.settings.No_Zeal_Shotgunner and VariousWeaponForCops.settings.Dont_Be_Annoying then
     if self._unit:base()._tweak_table == "heavy_swat_sniper" then
+    	addweapon(self._default_weapon_id,1)
+    elseif self._unit:base()._tweak_table == "city_swat" or self._unit:base()._tweak_table == "fbi_swat" or self._unit:base()._tweak_table == "fbi_heavy_swat" then
+        addweapon("mini",40)
+        addweapon("c45",10)
+    	addweapon("m249",75)
+    	addweapon("m4",50)
+    	addweapon("g36",125)
+    	addweapon("r870",150)
+    	addweapon("raging_bull",50)
+    	addweapon("mp5",50)
+    	addweapon("ak47",50)
+    	addweapon("saiga",50)
+    	addweapon("ump",25)
+    	addweapon("beretta92",20)
+    	addweapon("heavy_zeal_sniper",40)
+    	--addweapon("m14_sniper_npc",7)
+    	--addweapon("svdsil_snp",3)
+    	addweapon("rpk_lmg",25)
+    	addweapon("m4_yellow",30)
+    	addweapon("smoke",5)
+    	addweapon("mac11",15)
+    	addweapon("sg417",10)
+    	addweapon(self._default_weapon_id,180)
+    else
+    	addweapon("mini",8)
+    	addweapon("m249",40)
+    	addweapon("m4",50)
+    	addweapon("g36",202)
+    	--addweapon("r870",200)
+    	addweapon("raging_bull",50)
+    	addweapon("mp5",50)
+    	addweapon("ak47",50)
+    	addweapon("saiga",5)
+    	addweapon("ump",5)
+    	addweapon("beretta92",10)
+    	if self._unit:base()._tweak_table == "heavy_swat" then
+    	    addweapon("smoke",15)
+    	end
+    	addweapon(self._default_weapon_id,315)
+    end
+elseif VariousWeaponForCops.settings.Dont_Be_Annoying then
+	if self._unit:base()._tweak_table == "heavy_swat_sniper" then
+    	addweapon(self._default_weapon_id,1)
+    elseif self._unit:base()._tweak_table == "city_swat" or self._unit:base()._tweak_table == "fbi_swat" or self._unit:base()._tweak_table == "fbi_heavy_swat" then
+        addweapon("mini",40)
+        addweapon("c45",10)
+    	addweapon("m249",75)
+    	addweapon("m4",50)
+    	addweapon("g36",125)
+    	addweapon("r870",150)
+    	addweapon("raging_bull",50)
+    	addweapon("mp5",50)
+    	addweapon("ak47",50)
+    	addweapon("saiga",50)
+    	addweapon("ump",25)
+    	addweapon("beretta92",20)
+    	addweapon("heavy_zeal_sniper",40)
+    	--addweapon("m14_sniper_npc",7)
+    	--addweapon("svdsil_snp",3)
+    	addweapon("rpk_lmg",25)
+    	addweapon("m4_yellow",30)
+    	addweapon("smoke",5)
+    	addweapon("mac11",15)
+    	addweapon("sg417",10)
+    	addweapon(self._default_weapon_id,180)
+    else
+    	addweapon("mini",8)
+    	addweapon("m249",40)
+    	addweapon("m4",50)
+    	addweapon("g36",202)
+    	addweapon("r870",200)
+    	addweapon("raging_bull",50)
+    	addweapon("mp5",50)
+    	addweapon("ak47",50)
+    	addweapon("saiga",5)
+    	addweapon("ump",5)
+    	addweapon("beretta92",10)
+    	if self._unit:base()._tweak_table == "heavy_swat" then
+    	    addweapon("smoke",15)
+    	end
+    	addweapon(self._default_weapon_id,315)
+    end
+elseif VariousWeaponForCops.settings.No_Zeal_Shotgunner then
+	if self._unit:base()._tweak_table == "heavy_swat_sniper" then
     	addweapon(self._default_weapon_id,1)
     elseif self._unit:base()._tweak_table == "city_swat" or self._unit:base()._tweak_table == "fbi_swat" or self._unit:base()._tweak_table == "fbi_heavy_swat" then
         addweapon("mini",40)
@@ -164,7 +256,41 @@ end
 
 -- danger or not
 	if VariousWeaponForCops.settings.Dangerous_mode then
-        
+        local RiskFactorX = VariousWeaponForCops.settings.Risk_factor
+        local deck_id = managers.skilltree:get_specialization_value("current_specialization")
+        local deck_switch = {
+                    [15] = function()  --Anarchist
+                        if self._unit:base()._tweak_table == "city_swat" or self._unit:base()._tweak_table == "fbi_swat" or self._unit:base()._tweak_table == "fbi_heavy_swat" then
+                            addweapon("mini",RiskFactorX)
+                            addweapon("heavy_zeal_sniper",RiskFactorX)
+                        end
+                    end,
+                    [19] = function()  --Stoic
+                        addweapon("r870",RiskFactorX)
+                        addweapon("g36",RiskFactorX)
+                    end,
+                    [17] = function()  --Kingpin
+                        addweapon("r870",RiskFactorX)
+                    end
+                }
+        local check_deck = deck_switch[deck_id]
+
+        if check_deck then
+        	check_deck()
+        else
+        	log("Perk deck is not found.")
+        end
+
+        for _, peer in pairs(managers.network:session():peers()) do
+            local skill_data = managers.skilltree:unpack_from_string(peer:skills())
+            local perk_deck_index = skill_data.specializations[1]
+            local check_deck_client = deck_switch[perk_deck_index]
+            if check_deck_client then
+        	    check_deck_client()
+            else
+        	    log("Perk deck is not found!")
+            end
+        end
     end
 
 -- enable or not
